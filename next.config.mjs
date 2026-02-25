8f69724ef3afaf9166755c8531833551dea15f16
madhavan2025 <madhavan.sykasys@gmail.com>
2026-02-25 19:35:18 +0530

    Isbvwwdd

--- next.config.mjs
+++ next.config.mjs
@@ -5,30 +5,25 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
-        port: ""
       },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        port: ""
       },
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
-        port: ""
       },
       {
         protocol: "https",
         hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
-        port: ""
       },
       {
         protocol: "https",
-        hostname: "admin-weld-beta-21.vercel.app", // ✅ add this
-        port: ""
+        hostname: "*.public.blob.vercel-storage.com", // ✅ THIS is required
       }
-    ]
-  }
+    ],
+  },
 };
 
 export default nextConfig;
