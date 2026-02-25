9d08aa784ec02a650c41f96870d3116ef4a09ce4
madhavan2025 <madhavan.sykasys@gmail.com>
2026-02-25 18:19:29 +0530

    Is

--- src/app/forms/form-layout/_components/contact-form.tsx
+++ src/app/forms/form-layout/_components/contact-form.tsx
@@ -49,7 +49,7 @@ export function ContactForm() {
           ]}
         />
 
-        <TextAreaGroup label="Message" placeholder="Type your message" />
+        <TextAreaGroup name="defaultTextarea" label="Message" placeholder="Type your message" />
 
         <button className="mt-6 flex w-full justify-center rounded-lg bg-primary p-[13px] font-medium text-white hover:bg-opacity-90">
           Send Message
