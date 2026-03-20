1e9c02d63cf804b5cb984d7d7c19f3a4b8009ead
madhavan2025 <madhavan.sykasys@gmail.com>
2026-03-20 18:48:28 +0530

    upsundwwated changewww0000000saazaasssxxxwws

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -88,24 +88,7 @@ useEffect(() => {
               height: "auto",
             }}
           />
-          <div className="absolute bottom-1 right-1 z-10 xsm:bottom-4 xsm:right-4">
-            <label
-              htmlFor="cover"
-              className="flex cursor-pointer items-center justify-center gap-2 rounded-lg bg-primary px-[15px] py-[5px] text-body-sm font-medium text-white hover:bg-opacity-90"
-            >
-              <input
-                type="file"
-                name="coverPhoto"
-                id="coverPhoto"
-                className="sr-only"
-                onChange={handleChange}
-                accept="image/png, image/jpg, image/jpeg"
-              />
-
-
-
-            </label>
-          </div>
+        
         </div>
         <div className="px-4 pb-6 text-center lg:pb-8 xl:pb-11.5">
           <div className="relative z-30 mx-auto -mt-22 h-30 w-full max-w-30 rounded-full bg-white/20 p-1 backdrop-blur sm:h-44 sm:max-w-[176px] sm:p-3">
